5a503ce1de06d09da19d7c3661ca4ff619bd8990
Pablo Romero <romero7116@gmail.com>
2022-08-18 18:04:14 +0200

    Create initial project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
