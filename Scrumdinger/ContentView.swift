65910cb52b366a89a5b7afd0ec89984d7827fef3
Pablo Romero <romero7116@gmail.com>
2022-08-19 23:54:15 +0200

    Step: Using Stacks to Arrange Views

--- Scrumdinger/ContentView.swift
+++ Scrumdinger/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(/*@START_MENU_TOKEN@*/.caption/*@END_MENU_TOKEN@*/)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(
+                    action: { },
+                    label: {
+                        Image(systemName: "forward.fill")
+                    }
+                )
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView()
     }
 }
