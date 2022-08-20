e1371bb1e9a6a578b38d0a832954f7432dd0273d
Pablo Romero <romero7116@gmail.com>
2022-08-20 17:54:16 +0200

    Step: Creating a Navigation Hierarchy

--- Scrumdinger/Model/DailyScrum.swift
+++ Scrumdinger/Model/DailyScrum.swift
@@ -10,16 +10,32 @@ import Foundation
 struct DailyScrum: Identifiable {
     let id: UUID = UUID()
     let title: String
-    let attendees: [String]
+    let attendees: [Attendee]
     let lengthInMinutes: Int
     let theme: Theme
+
+    struct Attendee: Identifiable {
+        let id: UUID = UUID()
+        let name: String
+    }
 }
 
 extension DailyScrum {
-    static let sampleData: [DailyScrum] =
-    [
-        .init(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
-        .init(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
-        .init(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    static let sampleData: [DailyScrum] = [
+        .init(
+            title: "Design",
+            attendees: ["Cathy", "Daisy", "Simon", "Jonathan"].map(Attendee.init),
+            lengthInMinutes: 10,
+            theme: .yellow),
+        .init(
+            title: "App Dev",
+            attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"].map(Attendee.init),
+            lengthInMinutes: 5,
+            theme: .orange),
+        .init(
+            title: "Web Dev",
+            attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"].map(Attendee.init),
+            lengthInMinutes: 5,
+            theme: .poppy)
     ]
 }
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -11,12 +11,24 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
 
     var body: some View {
-        List {
-            ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
+        NavigationView {
+            List {
+                ForEach(scrums) { scrum in
+                    NavigationLink(destination: DetailView(scrum: scrum)) {
+                        CardView(scrum: scrum)
+                    }
                     .listRowBackground(scrum.theme.mainColor)
+                }
             }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+            }
+            .accessibilityLabel("New Scrum")
         }
+
     }
 }
 
